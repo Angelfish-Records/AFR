fb4309164f107d862effdae5ccd16e1ff02a81b3
Brendan McBryde <admin@brendanjohnroch.com>
2026-04-02 21:27:49 +1300

    add artist

--- lib/catalogue/api.ts
+++ lib/catalogue/api.ts
@@ -14,6 +14,7 @@ export function toCatalogueListItem(record: CatalogueRecord): CatalogueRecordLis
     id: record.id,
     recordingId: record.recordingId,
     title: record.title,
+    artistName: record.artistName,
     syncReadinessSummary: record.syncReadinessSummary,
     duration: record.duration,
     genreLabels: record.genreLabels,
